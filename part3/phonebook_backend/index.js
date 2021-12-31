512b96863c1d8826799692beccf7e092b88e8508
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-30 22:53:50 -0800

    add procfile

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -109,7 +109,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 // server runs locally on port 3001
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

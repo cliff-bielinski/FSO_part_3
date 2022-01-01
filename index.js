532193ca912053c1664cf0daa8c0843f35691958
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-31 23:26:41 -0800

    exercise 3.20 done

--- index.js
+++ index.js
@@ -72,7 +72,9 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  const opts = { runValidators: true, new: true }
+
+  Person.findByIdAndUpdate(request.params.id, person, opts)
     .then(updatedPerson => {
       response.json(updatedPerson)
     })

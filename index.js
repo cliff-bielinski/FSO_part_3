10e7e9641983cd0248e757e7013df200643a41c4
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-31 22:08:15 -0800

    exercise 3.17 done

--- index.js
+++ index.js
@@ -67,6 +67,22 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+//updates contact of given id in phonebook
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 // deletes contact of given id from phonebook
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
